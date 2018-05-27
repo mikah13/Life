596635cc03d15bf8d39b2e56c527d98105eadf95
mikah <anhminhhoang13@gmail.com>
2018-05-27 11:18:11 -0700

    Finish Index.html

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.sidenav').sidenav();
+    $('input.autocomplete').autocomplete({
+     data: {
+       "Apple": null,
+       "Microsoft": null,
+       "Google": 'https://placehold.it/250x250'
+     },
+   });
+});
