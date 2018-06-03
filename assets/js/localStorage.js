94ef708ca3b0e2492b7208b26609f6eefc44f5a8
mikah <anhminhhoang13@gmail.com>
2018-06-03 16:09:28 -0700

    Update

--- assets/js/localStorage.js
+++ assets/js/localStorage.js
@@ -1,9 +1,62 @@
-function validateLocal() {
-    return localStorage.getItem('account');
-}
-function setLocal() {
-    localStorage.setItem('account', true);
-}
-function clearLocal() {
-    localStorage.clear();
+// function clearLocal() {
+//     localStorage.clear();
+// }
+//
+// function addItem(a, b) {
+//     localStorage.setItem(a, b);
+// }
+// function removeItem(a){
+//     localStorage.removeItem(a);
+// }
+
+class Storage {
+    constructor() {
+        this.length = localStorage.length;
+    }
+    clear() {
+        localStorage.clear();
+        this.length = 0;
+    }
+
+    add(a, b) {
+        localStorage.setItem(a, b);
+        this.length++;
+    }
+    remove(a) {
+        localStorage.removeItem(a);
+        this.length--;
+    }
+    getList() {
+        let values = [],
+            keys = Object.keys(localStorage),
+            i = keys.length;
+
+        while (i--) {
+            values.push(localStorage.getItem(keys[i]));
+        }
+
+        return values;
+    }
+    itemDone(a) {
+        var keys = Object.keys(localStorage),
+            i = keys.length;
+
+        let obj = JSON.parse(localStorage.getItem(keys[i - a - 1]));
+        obj.done = true;
+        localStorage.setItem(keys[i - a - 1], JSON.stringify(obj))
+    }
+    itemUndo(a) {
+        var keys = Object.keys(localStorage),
+            i = keys.length;
+
+        let obj = JSON.parse(localStorage.getItem(keys[i - a - 1]));
+        obj.done = false;
+        localStorage.setItem(keys[i - a - 1], JSON.stringify(obj))
+    }
+    delete(a) {
+        var keys = Object.keys(localStorage),
+            i = keys.length;
+
+        this.remove(keys[i - a - 1]);
+    }
 }
